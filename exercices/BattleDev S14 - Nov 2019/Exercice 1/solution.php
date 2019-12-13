4bb85297f8168d54b79734584da7ec5c03bb5695
COLAS Fabien <colas.fab@gmail.com>
2019-12-13 11:41:31 +0100

    Rajout de la solution BattleDev S12 exercice 1

--- exercices/BattleDev S14 - Nov 2019/Exercice 1/solution.php
+++ exercices/BattleDev S14 - Nov 2019/Exercice 1/solution.php
@@ -6,7 +6,21 @@ require_once 'run.php';
 // Implémentez votre code ci-dessous
 function ContestResponse($input) 
 {
-    // Mon code ici
+    // Implémentez votre code ci-dessous
+    $nbParticipant = $input[0];
+    $valeurMin = 10001;
+    $nomPersonne = "";
+
+    for($i = 1; $i <= $nbParticipant; $i++):
+        $tableTemps = explode(" ", $input[$i]);
+        
+        if( $valeurMin > $tableTemps[1] ) :
+            $nomPersonne = $tableTemps[0];
+            $valeurMin = $tableTemps[1];
+        endif;
+    endfor;
+
+    echo $nomPersonne;
 }
 // -- Fin de votre code
 
